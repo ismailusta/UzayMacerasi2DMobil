93baebbbe6b9743f9fdc4c54d6d3b69e0949c621
ismailusta <ismail.usta028@gmail.com>
2025-03-29 04:51:19 +0300

    joystick eklencek

---
+++ UzayMacerasi/Assets/Scripts/YuksekSkor/EnYuksekSkorKontrol.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnYuksekSkorKontrol : MonoBehaviour
+{
+    [SerializeField] Button _geriDonButon;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _geriDonButon.onClick.AddListener(() => AnaMenuyeDon());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void AnaMenuyeDon()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
